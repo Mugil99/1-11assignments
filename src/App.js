fc708c31528c767de63f5d79ba411c7fff646b2d
Mugil99 <mugilya99@gmail.com>
2023-06-10 14:27:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Tab from './Components/Tab';
+import { useState } from 'react';
 function App() {
+  var arr=[{title:"Tab1",content:"This is content for Tab 1"},
+  {title:"Tab2",content:"This is content for Tab 2"},
+  {title:"Tab3",content:"This is content for Tab 3"},    
+]
+const [cont,setCont]=useState("");
+const displayContent=(index)=>{
+   setCont(arr[index].content)
+   console.log(cont);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {arr.map((obj,index)=>{
+
+          return(
+            <li onClick={()=>{displayContent(index)}}>
+            <Tab title={obj.title} content={obj.content} key={index} />
+            </li>
+          )
+        })}
+      </ul>
+      <p>{cont}</p>
     </div>
   );
 }
---
+++ src/Components/Tab.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Tab=(props)=>{
+    console.log(props);
+   
+    return(
+        <div>
+            {props.title}
+        </div>
+    )
+}
+export default Tab;
